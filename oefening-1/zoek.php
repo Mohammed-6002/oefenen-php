20fb430fb2f7ca6d51b105d74d5679531f0fab4b
Mohammed-6002 <302352550@student.rocmondriaan.nl>
2025-05-26 13:26:31 +0200

    oefening 1

--- oefening-1/zoek.php
+++ oefening-1/zoek.php
@@ -1,5 +1,4 @@
 <?php
-// Check if 'zoekwoord' is set in the URL query string
 if (isset($_GET['zoekwoord'])) {
     $zoekwoord = htmlspecialchars($_GET['zoekwoord']);
     echo "Je zoekt naar: " . $zoekwoord;
